5bf5193a554c2eb6205722ba688612891f298dc8
Samson <samson.nguyen@mavs.uta.edu>
2022-04-07 19:42:49 -0500

    can't figure out accuracy unu

--- assignment6/month_solution.py
+++ assignment6/month_solution.py
@@ -3,6 +3,7 @@
 # CSE 4392 Assignment 6
 import numpy as np
 from tensorflow import keras
+from keras.utils.np_utils import to_categorical
 
 
 def data_normalization(raw_data, train_start, train_end):
@@ -22,8 +23,9 @@ def data_normalization(raw_data, train_start, train_end):
 def make_inputs_and_targets(data, months, size, sampling):
     num_observations = 14 * 24 * 6 // sampling
     inputs = np.zeros((size, num_observations, 14))
-    targets = np.zeros(size)
+    targets = np.zeros((size, 12))
     counter = 0
+    months_categorical = to_categorical(months, dtype="uint8")
     # rand_input = set()
     # while len(rand_input) < size:
     #     rand_input.add(np.random.randint(0, len(data - 14 * 24 * 6)))
@@ -32,8 +34,8 @@ def make_inputs_and_targets(data, months, size, sampling):
         x = []
         for n in range(i, i + 14 * 24 * 6, sampling):
             x.append(data[n])
-        inputs[counter] = x
-        targets[counter] = months[i + (num_observations // 2)]
+        inputs[counter] = x.copy()
+        targets[counter] = months_categorical[i + (num_observations // 2)]
         counter += 1
         if counter == size:
             break
@@ -45,25 +47,19 @@ def build_and_train_dense(train_inputs, train_targets, val_inputs, val_targets, 
     model = keras.Sequential([keras.Input(shape=input_shape),
                               keras.layers.Flatten(),
                               keras.layers.Dense(64, activation="tanh"),
-                              # keras.layers.Dropout(0.2, input_shape=(64,)),
-                              # keras.layers.Dropout(0.2),
                               keras.layers.Dense(32, activation="tanh"),
-                              # keras.layers.Dense(32, activation="tanh"),
-                              keras.layers.Dense(12, activation="tanh"),
-                              # keras.layers.LSTM(16),
-                              # keras.layers.Flatten(),
-                              keras.layers.Dropout(0.2),
-                              keras.layers.Dense(12, activation="tanh")
+                              # keras.layers.Dense(16, activation="tanh"),
+                              # keras.layers.Dropout(0.2),
+                              keras.layers.Dense(12)
                               ])
-    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics='accuracy')
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accuracy')
+    # model.compile(loss='mse', optimizer='adam', metrics='accuracy')
     callbacks = [keras.callbacks.ModelCheckpoint(filepath=filename, monitor='val_accuracy', save_best_only=True)]
     training_history = model.fit(train_inputs,
                                  train_targets,
                                  epochs=10,
                                  validation_data=(val_inputs, val_targets),
-                                 # validation_steps=10,
                                  callbacks=callbacks)
-    # model.save(filename)
     return training_history
 
 
@@ -72,11 +68,19 @@ def build_and_train_rnn(train_inputs, train_targets, val_inputs, val_targets, fi
     model = keras.Sequential([keras.Input(shape=input_shape),
                               keras.layers.Bidirectional(keras.layers.LSTM(32)),
                               # from here on you decide what to do, there are multiple correct options
+                              # keras.layers.Bidirectional(keras.layers.LSTM(32)),
+                              keras.layers.Dense(64, activation="relu"),
+                              keras.layers.Dense(32, activation="relu"),
+                              keras.layers.Dense(16, activation="relu"),
+                              # keras.layers.Dense(12)
+                              keras.layers.Dense(12)
                               ])
     callbacks = [keras.callbacks.ModelCheckpoint(filepath=filename, monitor='val_accuracy', save_best_only=True)]
-    model.compile(loss='mse', optimizer='adam', metrics='accuracy')
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accuracy')
+    # model.compile(loss='mse', optimizer='adam', metrics='accuracy')
     training_history = model.fit(train_inputs,
                                  train_targets,
+                                 epochs=10,
                                  validation_data=(val_inputs, val_targets),
                                  callbacks=callbacks)
     return training_history
@@ -89,7 +93,8 @@ def test_model(filename, test_inputs, test_targets):
     # temp = np.expand_dims(temp, axis=3)
     # small_test_inputs = np.repeat(temp, 3, axis=3)
     # evaluation
-    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+    # model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     loss, acc = model.evaluate(temp, test_targets)
     return acc
 
@@ -97,8 +102,9 @@ def test_model(filename, test_inputs, test_targets):
 def confusion_matrix(filename, test_inputs, test_targets):
     matrix = np.zeros((12, 12))
     model = keras.models.load_model(filename)
-    predictions = model.predict(test_inputs)
+    pd = model.predict(test_inputs)
+    predictions = [np.argmax(pd[i]) for i in range(len(test_inputs))]
     for i in range(len(predictions)):
-        if np.argmax(predictions[i]) != np.argmax(test_targets[i]):
-            matrix[test_targets[i], predictions[i]] += 1
-    return
+        if predictions[i] != np.argmax(test_targets[i]):
+            matrix[np.argmax(test_targets[i]), predictions[i]] += 1
+    return matrix
